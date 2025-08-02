0adbf9549261e603c139feb59e0aca5da04ce081
93094049+kraemr@users.noreply.github.com <93094049+kraemr@users.noreply.github.com>
2025-08-02 13:13:10 +0200

    cleaned up config.c

--- config.c
+++ config.c
@@ -6,28 +6,12 @@
 #include "tinyusb/src/tusb.h"
 #include <stdint.h>
 
-// if 0 then script will immediately execute.
-// Otherwise the board button has to be pressed
-const uint8_t BOARD_BUTTON_CONFIRMATION_NEEDED = 1;
-
-// if 0 Script runs once, else it repeatedly executes it
-const uint8_t REPEAT_DUCKY_SCRIPT = 0;
-
-// Set this according to the PC that it will be attached to
-// a sensible default delay would be 125ms
-const uint16_t KEYPRESS_DELAY_MS = 125;
-
-//######## CONFIG ##################
-
-// IMPORTANT PUT THE LINE, COUNT OF YOUR PAYLOAD HERE
-#define MAIN_PAYLOAD_LEN_DEF 2
-const int MAIN_PAYLOAD_LEN=MAIN_PAYLOAD_LEN_DEF;
 /*
-The HID_KEYs as they are defined in tinyusb are for the US Layout
-IF the PC doesnt use US Layout, then you will have to lookup us layout and convert the keys
-For example on German Keyboard layout "EQ" in your Payload will lead to the Gravure Key being pressed instead of =
-For Most Layouts finding the correct values to use can be easy or NOT
-But the most common layouts should work without issues
+    The HID_KEYs as they are defined in tinyusb are for the US Layout
+    IF the PC doesnt use US Layout, then you will have to lookup us layout and convert the keys
+    For example on German Keyboard layout "EQ" in your Payload will lead to the Gravure Key being pressed instead of =
+    For The US Layout a header file is pre-included at layouts/us_keyboard_layout.h
+    Feel free to add your own layouts!
 */
 
 #define L_CTRL    0b00000001
@@ -44,8 +28,37 @@ But the most common layouts should work without issues
 #define MOUSE_RIGHT 2
 #define MOUSE_MDIILE 3
 
-// Special Key < on German Keyboards and other european ones for üshould be:
-// #define HID_KEY_EUROPE_2 0x64
+
+// if 0 then script will immediately execute.
+// Otherwise the board button has to be pressed
+const uint8_t BOARD_BUTTON_CONFIRMATION_NEEDED = 1;
+
+// if 0 Script runs once, else it repeatedly executes it
+const uint8_t REPEAT_DUCKY_SCRIPT = 0;
+
+// Set this according to the PC that it will be attached to
+// a sensible default delay would be 125ms
+const uint16_t KEYPRESS_DELAY_MS = 125;
+
+//######## CONFIG ##################
+
+// IMPORTANT PUT THE LINE, COUNT OF YOUR PAYLOAD HERE
+#define MAIN_PAYLOAD_LEN_DEF 2
+const int MAIN_PAYLOAD_LEN=MAIN_PAYLOAD_LEN_DEF;
+
+/*
+This is a pretty barebones approach, where what you put in here directly gets sent.
+You can use these descriptors from usb_descriptors:
+enum
+{
+  REPORT_ID_KEYBOARD = 1,
+  REPORT_ID_MOUSE,
+  REPORT_ID_CONSUMER_CONTROL,
+  REPORT_ID_GAMEPAD,
+  REPORT_ID_COUNT
+};
+
+*/
 const uint8_t MAIN_PAYLOAD[MAIN_PAYLOAD_LEN_DEF][8]={
     {REPORT_ID_KEYBOARD,0x00,DUCK_KEY_0,0,0,0,0,0},
     {REPORT_ID_KEYBOARD,0x00,DUCK_KEY_1,0,0,0,0,0},
